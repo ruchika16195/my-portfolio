5879681e6f56b19187d77d32f0d8c20c0f99bcbb
ruchika16195 <86045806+ruchika16195@users.noreply.github.com>
2026-03-07 14:39:46 +0000

    Updating portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+
+  const currentTheme =
+    document.documentElement.getAttribute('data-theme');
+
+  if (currentTheme === "dark") {
+    document.documentElement.setAttribute('data-theme', 'light');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+
+}
